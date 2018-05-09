1b3fc6d3d861c287661563ca3c662f542a1d10ea
Matias Teragni <matias.teragni@gmail.com>
2018-05-09 16:23:03 -0300

    Small test on how it parsed functions. parameters, and globals

--- MemSQL/MemSQL.TestUI/Program.cs
+++ MemSQL/MemSQL.TestUI/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,11 @@ namespace MemSQL.TestUI
     {
         static void Main(string[] args)
         {
+            Microsoft.SqlServer.TransactSql.ScriptDom.TSql140Parser parser = new Microsoft.SqlServer.TransactSql.ScriptDom.TSql140Parser(false);
+
+            var reader = new StringReader("Insert into tbl(A,B,C) values (SCOPE_IDENTITY() ,@@Identity,@P)"); 
+            var result = parser.Parse(reader,out var error);
+            Console.ReadLine();
         }
     }
 }
