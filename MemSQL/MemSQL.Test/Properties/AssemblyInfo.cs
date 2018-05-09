45f57d99fd46a0116a13059aabbb1ce1600bde32
RichoM <richi.moran@gmail.com>
2018-05-09 17:50:31 -0300

    Added test project.

---
+++ MemSQL/MemSQL.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MemSQL.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MemSQL.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c8cc24cf-69c5-4413-a868-460c3db31727")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ MemSQL/MemSQL.Test/UnitTest1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MemSQL;
+using System.Collections.Generic;
+using System.Data;
+
+namespace MemSQL.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TableCreationTest()
+        {
+            string script = "Create table [TBL](col1 int,col2 varchar(3),col3 bit)";
+            DataSet ds = new DataSet();
+            var visitor = new SQLInterpreter(ds);
+            int rows = visitor.Execute(script);
+            Assert.IsTrue(ds.Tables.Contains("TBL"), "The table must be created");
+            var table = ds.Tables["TBL"];
+            Assert.IsTrue(table.Columns.Contains("col1"));
+            Assert.AreEqual(typeof(int), table.Columns["col1"].DataType);
+            Assert.IsTrue(table.Columns.Contains("col2"));
+            Assert.AreEqual(typeof(string), table.Columns["col2"].DataType);
+            Assert.IsTrue(table.Columns.Contains("col3"));
+            Assert.AreEqual(typeof(bool), table.Columns["col3"].DataType);
+        }
+    }
+}
