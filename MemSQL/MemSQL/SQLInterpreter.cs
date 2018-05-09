ba38770546f5c63dec747b6dd546333acda9c166
RichoM <richi.moran@gmail.com>
2018-05-09 17:50:22 -0300

    Basic implementation of SQLInterpreter.Execute(..)

--- MemSQL/MemSQL/SQLInterpreter.cs
+++ MemSQL/MemSQL/SQLInterpreter.cs
@@ -1,12 +1,33 @@
-﻿using System;
+﻿using Microsoft.SqlServer.TransactSql.ScriptDom;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MemSQL
 {
-    class SQLInterpreter : SQLTemporalInterpreter
+    public class SQLInterpreter : SQLTemporalInterpreter
     {
+        private DataSet ds;
+
+        public SQLInterpreter(DataSet ds)
+        {
+            this.ds = ds;
+            
+        }
+
+        public int Execute(string script)
+        {
+            var parser = new TSql140Parser(false);
+            TSqlScript result = (TSqlScript)parser.Parse(new StringReader(script), out var errors);
+            foreach (var batch in result.Batches)
+            {
+                ExplicitVisit(batch);
+            }
+            return 1;
+        }
     }
 }
