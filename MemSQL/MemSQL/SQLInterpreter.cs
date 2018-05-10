d6796114ec7eb8a81fcfe6904706fce61bb6d87a
Matias Teragni <matias.teragni@gmail.com>
2018-05-09 21:04:21 -0300

    Added a class to hold the explicit visit methods, those determine the order in which the nodes are visited. I am not happy with the name i chose so i'll probably change it later on

--- MemSQL/MemSQL/SQLInterpreter.cs
+++ MemSQL/MemSQL/SQLInterpreter.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace MemSQL
 {
-    public class SQLInterpreter : SQLTemporalInterpreter
+    public class SQLInterpreter : SQLVisitor
     {
         private Stack<object> stack;
         private DataSet ds;
@@ -35,43 +35,20 @@ namespace MemSQL
         private T pop<T>() { return (T)(stack.Pop()); }
 
 
-        public override void ExplicitVisit(CreateTableStatement node)
-        {
-            //node.Definition contains what i need.
-            //i can define the visiting order here, or just use the one provided by the implementation of the other visitor
-            //ExplicitVisit(node.Definition);
-            //ExplicitVisit(node.SchemaObjectName);
-
-            node.AcceptChildren(this);
-
-            Visit(node);
-
-        }
-
         public override void Visit(CreateTableStatement node)
         {
             //TODO: creation errors? what if the name is taken?
             int columnCount = pop<int>();
 
             DataColumn[] columns = new DataColumn[columnCount];
-            for (int i = columnCount-1; i >= 0; i--)
+            for (int i = columnCount - 1; i >= 0; i--)
             {
                 columns[i] = new DataColumn(pop<string>(), pop<Type>());
             }
             DataTable t = new DataTable(pop<string>());
             t.Columns.AddRange(columns);
             ds.Tables.Add(t);
-           
-        }
 
-
-
-
-        public override void ExplicitVisit(SchemaObjectName node)
-        {
-            //not checking the childs for now
-            //TODO: check the childs
-            Visit(node);
         }
 
         public override void Visit(SchemaObjectName node)
@@ -81,35 +58,18 @@ namespace MemSQL
             push(node.Identifiers[0].Value);
             base.Visit(node);
         }
-        public override void ExplicitVisit(TableDefinition node)
+        public override void Visit(TableDefinition node)
         {
-            //TODO: indexes, constraints
-            foreach (var item in node.ColumnDefinitions)
-            {
-                item.Accept(this);
-            }
-            Visit(node);
-        }
-        public override void Visit(TableDefinition node) {
             push(node.ColumnDefinitions.Count);
         }
 
-        public override void ExplicitVisit(ColumnDefinition node)
+        public override void Visit(ColumnDefinition node)
         {
-            //if i dont override this it tries to call visit columndefinitionbase
-            node.DataType.Accept(this);
-            Visit(node);
-        }
-        public override void Visit(ColumnDefinition node) {
             //TODO: identity, collation,indexes, etc,calcultaed values?
             //solution for now, tuple
             push(node.ColumnIdentifier.Value);
-         }
-
-        public override void ExplicitVisit(SqlDataTypeReference node)
-        {
-            Visit(node);
         }
+
 
         public override void Visit(SqlDataTypeReference node)
         {
@@ -129,66 +89,66 @@ namespace MemSQL
                     break;
 
                 case SqlDataTypeOption.None:
-                    
+
                 case SqlDataTypeOption.BigInt:
-                    
+
                 case SqlDataTypeOption.SmallInt:
-                    
+
                 case SqlDataTypeOption.TinyInt:
-                    
-                    
+
+
                 case SqlDataTypeOption.Decimal:
-                    
+
                 case SqlDataTypeOption.Numeric:
-                    
+
                 case SqlDataTypeOption.Money:
-                    
+
                 case SqlDataTypeOption.SmallMoney:
-                    
+
                 case SqlDataTypeOption.Float:
-                    
+
                 case SqlDataTypeOption.Real:
-                    
+
                 case SqlDataTypeOption.DateTime:
-                    
+
                 case SqlDataTypeOption.SmallDateTime:
-                    
+
                 case SqlDataTypeOption.Char:
-                    
+
                 case SqlDataTypeOption.Text:
-                    
+
                 case SqlDataTypeOption.NChar:
-                    
+
                 case SqlDataTypeOption.NVarChar:
-                    
+
                 case SqlDataTypeOption.NText:
-                    
+
                 case SqlDataTypeOption.Binary:
-                    
+
                 case SqlDataTypeOption.VarBinary:
-                    
+
                 case SqlDataTypeOption.Image:
-                    
+
                 case SqlDataTypeOption.Cursor:
-                    
+
                 case SqlDataTypeOption.Sql_Variant:
-                    
+
                 case SqlDataTypeOption.Table:
-                    
+
                 case SqlDataTypeOption.Timestamp:
-                    
+
                 case SqlDataTypeOption.UniqueIdentifier:
-                    
+
                 case SqlDataTypeOption.Date:
-                    
+
                 case SqlDataTypeOption.Time:
-                    
+
                 case SqlDataTypeOption.DateTime2:
-                    
+
                 case SqlDataTypeOption.DateTimeOffset:
-                    
+
                 case SqlDataTypeOption.Rowversion:
-                    
+
                 default:
                     throw new NotImplementedException();
 
---
+++ MemSQL/MemSQL/SQLVisitor.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.SqlServer.TransactSql.ScriptDom;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemSQL
+{
+    public abstract class SQLVisitor : SQLTemporalInterpreter
+    { 
+
+        public override void ExplicitVisit(CreateTableStatement node)
+        {
+            //node.Definition contains what i need.
+            //i can define the visiting order here, or just use the one provided by the implementation of the other visitor
+            //ExplicitVisit(node.Definition);
+            //ExplicitVisit(node.SchemaObjectName);
+
+            node.AcceptChildren(this);
+
+            Visit(node);
+
+        }
+
+        public override void ExplicitVisit(SchemaObjectName node)
+        {
+            //not checking the childs for now
+            //TODO: check the childs
+            Visit(node);
+        }
+        public override void ExplicitVisit(TableDefinition node)
+        {
+            //TODO: indexes, constraints
+            foreach (var item in node.ColumnDefinitions)
+            {
+                item.Accept(this);
+            }
+            Visit(node);
+        }
+        public override void ExplicitVisit(ColumnDefinition node)
+        {
+            //if i dont override this it tries to call visit columndefinitionbase
+            node.DataType.Accept(this);
+            Visit(node);
+        }
+        public override void ExplicitVisit(SqlDataTypeReference node)
+        {
+            Visit(node);
+        }
+
+    }
+}
