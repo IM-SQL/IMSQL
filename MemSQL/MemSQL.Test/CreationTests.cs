e171805a9780a866b4ad3cd4663fd7271430ed1f
RichoM <richi.moran@gmail.com>
2018-05-13 15:35:04 -0300

    Added test.

--- MemSQL/MemSQL.Test/CreationTests.cs
+++ MemSQL/MemSQL.Test/CreationTests.cs
@@ -416,5 +416,33 @@ namespace MemSQL.Test.Structural
                 Assert.IsFalse(col.AllowDBNull, "The column should not allow null");
             }
         }
+
+        [TestMethod]
+        public void RecursiveFKShouldWork()
+        {
+            string script = @"
+                CREATE TABLE [dbo].[LogException] 
+                (
+                    [id]             INT             IDENTITY (1, 1) NOT NULL,
+                    [name]           NVARCHAR (512)  NOT NULL,
+                    [message]        NVARCHAR (512)  NULL,
+                    [stackTrace]     NVARCHAR (4000) NULL,
+                    [source]         NVARCHAR (256)  NULL,
+                    [innerException] INT             NULL,
+                    PRIMARY KEY CLUSTERED ([id] ASC),
+                    CONSTRAINT [FK_LogException_ToLogException] FOREIGN KEY ([innerException]) REFERENCES [dbo].[LogException] ([id])
+                )";
+            DataSet ds = new DataSet();
+            var visitor = new SQLInterpreter(ds);
+            visitor.Execute(script);
+
+            var table = ds.Tables["LogException"];
+            Assert.IsNotNull(table, "The table should exist");
+            CollectionAssert.AreEqual(new[] { "id" },
+                table.PrimaryKey.Select(c => c.ColumnName).ToArray(),
+                "The PK should be configured correctly");
+
+            Assert.IsTrue(table.Constraints.Contains("FK_LogException_ToLogException"));
+        }
     }
 }
