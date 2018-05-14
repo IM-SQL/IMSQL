9c0d2dc6b567a6df7b2215218f70b561154e421a
RichoM <richi.moran@gmail.com>
2018-05-14 16:22:50 -0300

    Renamed Form1 to MainForm.

--- MemSQL/MemSQL.REPL/MainForm.Designer.cs
+++ MemSQL/MemSQL.REPL/MainForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace MemSQL.REPL
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// Required designer variable.
--- MemSQL/MemSQL.REPL/Program.cs
+++ MemSQL/MemSQL.REPL/Program.cs
@@ -16,7 +16,7 @@ namespace MemSQL.REPL
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
