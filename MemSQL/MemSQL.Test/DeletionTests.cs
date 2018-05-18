a350bf4592ddabdbceed29df18a3d1b425bcc7d3
Matias Teragni <matias.teragni@gmail.com>
2018-05-18 16:35:42 -0300

    Added test for a clear of the table

--- MemSQL/MemSQL.Test/DeletionTests.cs
+++ MemSQL/MemSQL.Test/DeletionTests.cs
@@ -22,16 +22,38 @@ namespace MemSQL.Test
             row["ID"] = 3;
             table.Rows.Add(row);
 
-
             string query = "Delete from [TBL]";
-
             SQLInterpreter interpreter = new SQLInterpreter(ds);
+
             var result = interpreter.Execute(query);
             int affected = result.RowsAffected;
-
             Assert.AreEqual(1, affected, "There should be one row affected");
             Assert.AreEqual(0, table.Rows.Count, "There should be no rows on the table");
             Assert.IsTrue(row.RowState == DataRowState.Detached, "The created row should have been detached because it is no longer in the table");
         }
+        [TestMethod]
+        public void BatchDeleteTest()
+        {
+            DataSet ds = new DataSet();
+            DataTable table = ds.Tables.Add("TBL");
+            table.Columns.Add(new DataColumn("ID", typeof(int)));
+            for (int i = 0; i < 100; i++)
+            {
+
+                var row = table.NewRow();
+                row["ID"] = i;
+                table.Rows.Add(row);
+
+            }
+            string query = "Delete from [TBL]";
+            SQLInterpreter interpreter = new SQLInterpreter(ds);
+
+            var result = interpreter.Execute(query);
+            int affected = result.RowsAffected;
+            Assert.AreEqual(100, affected, "There should be 100 row affected");
+            Assert.AreEqual(0, table.Rows.Count, "There should be no rows on the table");
+            }
+
+
     }
 }
