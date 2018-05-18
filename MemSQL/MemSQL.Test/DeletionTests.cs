e391231aed71ce6a24a494625653b868b2174e86
Matias Teragni <matias.teragni@gmail.com>
2018-05-18 16:41:01 -0300

    Added test for TOP qualifier on Delete

--- MemSQL/MemSQL.Test/DeletionTests.cs
+++ MemSQL/MemSQL.Test/DeletionTests.cs
@@ -17,7 +17,7 @@ namespace MemSQL.Test
             DataSet ds = new DataSet();
             DataTable table = ds.Tables.Add("TBL");
             table.Columns.Add(new DataColumn("ID", typeof(int)));
-            
+
             var row = table.NewRow();
             row["ID"] = 3;
             table.Rows.Add(row);
@@ -52,8 +52,46 @@ namespace MemSQL.Test
             int affected = result.RowsAffected;
             Assert.AreEqual(100, affected, "There should be 100 row affected");
             Assert.AreEqual(0, table.Rows.Count, "There should be no rows on the table");
+        }
+        [TestMethod]
+        public void TopDeleteTest()
+        {
+            DataSet ds = new DataSet();
+            DataTable table = ds.Tables.Add("TBL");
+            table.Columns.Add(new DataColumn("ID", typeof(int)));
+            for (int i = 0; i < 200; i++)
+            {
+                var row = table.NewRow();
+                row["ID"] = i;
+                table.Rows.Add(row);
             }
+            string query = "Delete TOP(50) from [TBL]";
+            SQLInterpreter interpreter = new SQLInterpreter(ds);
 
+            var result = interpreter.Execute(query);
+            int affected = result.RowsAffected;
+            Assert.AreEqual(50, affected, "There should be 50 row affected");
+            Assert.AreEqual(150, table.Rows.Count, "There should be 150 rows on the table");
+        }
+        [TestMethod]
+        public void TopPercentDeleteTest()
+        {
+            DataSet ds = new DataSet();
+            DataTable table = ds.Tables.Add("TBL");
+            table.Columns.Add(new DataColumn("ID", typeof(int)));
+            for (int i = 0; i < 200; i++)
+            {
+                var row = table.NewRow();
+                row["ID"] = i;
+                table.Rows.Add(row);
+            }
+            string query = "Delete TOP(50) PERCENT from [TBL]";
+            SQLInterpreter interpreter = new SQLInterpreter(ds);
 
+            var result = interpreter.Execute(query);
+            int affected = result.RowsAffected;
+            Assert.AreEqual(100, affected, "There should be 100 row affected");
+            Assert.AreEqual(100, table.Rows.Count, "There should be 100 rows on the table");
+        }
     }
 }
