1a45454fba60d39faba19e8377a968169c2a0f1a
Matias Teragni <matias.teragni@gmail.com>
2018-05-21 16:58:53 -0300

    Added test for OR

--- MemSQL/MemSQL.Test/DeletionTests.cs
+++ MemSQL/MemSQL.Test/DeletionTests.cs
@@ -336,5 +336,25 @@ namespace MemSQL.Test
             Assert.AreEqual(3, affected, "There should be 3 row affected");
             Assert.AreEqual(97, table.Rows.Count, "There should be 97 rows on the table");
         }
+        [TestMethod]
+        public void DeleteWhereOr()
+        {
+            DataSet ds = new DataSet();
+            DataTable table = ds.Tables.Add("TBL");
+            table.Columns.Add(new DataColumn("ID", typeof(int)));
+            for (int i = 0; i < 100; i++)
+            {
+                var row = table.NewRow();
+                row["ID"] = i;
+                table.Rows.Add(row);
+            }
+            string query = "Delete from [TBL] where [ID] < 1 OR [ID] > 98";
+            SQLInterpreter interpreter = new SQLInterpreter(ds);
+
+            var result = interpreter.Execute(query);
+            int affected = result.RowsAffected;
+            Assert.AreEqual(2, affected, "There should be 2 row affected");
+            Assert.AreEqual(98, table.Rows.Count, "There should be 98 rows on the table");
+        }
     }
 }
