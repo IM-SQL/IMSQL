314e079c5aaf1183450000657aa7a7683fe9ed0b
Matias Teragni <matias.teragni@gmail.com>
2018-05-21 16:36:59 -0300

    Added tests for the other conditional comparations

--- MemSQL/MemSQL.Test/DeletionTests.cs
+++ MemSQL/MemSQL.Test/DeletionTests.cs
@@ -133,6 +133,46 @@ namespace MemSQL.Test
             Assert.AreEqual(1, affected, "There should be 1 row affected");
             Assert.AreEqual(99, table.Rows.Count, "There should be 99 rows on the table");
         }
+        [TestMethod]
+        public void DeleteWhereNotEqualsE()
+        {
+            DataSet ds = new DataSet();
+            DataTable table = ds.Tables.Add("TBL");
+            table.Columns.Add(new DataColumn("ID", typeof(int)));
+            for (int i = 0; i < 100; i++)
+            {
+                var row = table.NewRow();
+                row["ID"] = i;
+                table.Rows.Add(row);
+            }
+            string query = "Delete from [TBL] where [ID] != 1";
+            SQLInterpreter interpreter = new SQLInterpreter(ds);
+
+            var result = interpreter.Execute(query);
+            int affected = result.RowsAffected;
+            Assert.AreEqual(99, affected, "There should be 99 row affected");
+            Assert.AreEqual(1, table.Rows.Count, "There should be 1 rows on the table");
+        }
+        [TestMethod]
+        public void DeleteWhereNotEqualsB()
+        {
+            DataSet ds = new DataSet();
+            DataTable table = ds.Tables.Add("TBL");
+            table.Columns.Add(new DataColumn("ID", typeof(int)));
+            for (int i = 0; i < 100; i++)
+            {
+                var row = table.NewRow();
+                row["ID"] = i;
+                table.Rows.Add(row);
+            }
+            string query = "Delete from [TBL] where [ID] <> 1";
+            SQLInterpreter interpreter = new SQLInterpreter(ds);
+
+            var result = interpreter.Execute(query);
+            int affected = result.RowsAffected;
+            Assert.AreEqual(99, affected, "There should be 99 row affected");
+            Assert.AreEqual(1, table.Rows.Count, "There should be 1 rows on the table");
+        }
 
         [TestMethod]
         public void DeleteWhereLT()
@@ -188,6 +228,46 @@ namespace MemSQL.Test
                 table.Rows.Add(row);
             }
             string query = "Delete from [TBL] where [ID] <= 2";
+            SQLInterpreter interpreter = new SQLInterpreter(ds);
+
+            var result = interpreter.Execute(query);
+            int affected = result.RowsAffected;
+            Assert.AreEqual(3, affected, "There should be 3 row affected");
+            Assert.AreEqual(97, table.Rows.Count, "There should be 97 rows on the table");
+        }
+        [TestMethod]
+        public void DeleteWhereNLT()
+        {
+            DataSet ds = new DataSet();
+            DataTable table = ds.Tables.Add("TBL");
+            table.Columns.Add(new DataColumn("ID", typeof(int)));
+            for (int i = 0; i < 100; i++)
+            {
+                var row = table.NewRow();
+                row["ID"] = i;
+                table.Rows.Add(row);
+            }
+            string query = "Delete from [TBL] where [ID] !< 97";
+            SQLInterpreter interpreter = new SQLInterpreter(ds);
+
+            var result = interpreter.Execute(query);
+            int affected = result.RowsAffected;
+            Assert.AreEqual(3, affected, "There should be 3 row affected");
+            Assert.AreEqual(97, table.Rows.Count, "There should be 97 rows on the table");
+        }
+        [TestMethod]
+        public void DeleteWhereNGT()
+        {
+            DataSet ds = new DataSet();
+            DataTable table = ds.Tables.Add("TBL");
+            table.Columns.Add(new DataColumn("ID", typeof(int)));
+            for (int i = 0; i < 100; i++)
+            {
+                var row = table.NewRow();
+                row["ID"] = i;
+                table.Rows.Add(row);
+            }
+            string query = "Delete from [TBL] where [ID] !> 2";
             SQLInterpreter interpreter = new SQLInterpreter(ds);
 
             var result = interpreter.Execute(query);
