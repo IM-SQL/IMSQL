6e68af1d7bae0d9f9885bece933cea8d65f21737
Matias Teragni <matias.teragni@gmail.com>
2018-05-21 15:02:38 -0300

    Added test

--- MemSQL/MemSQL.Test/DeletionTests.cs
+++ MemSQL/MemSQL.Test/DeletionTests.cs
@@ -112,5 +112,28 @@ namespace MemSQL.Test
             Assert.AreEqual(100, affected, "There should be 100 row affected");
             Assert.AreEqual(100, table.Rows.Count, "There should be 100 rows on the table");
         }
+
+        [TestMethod]
+        public void DeleteWhereEquals()
+        {
+            DataSet ds = new DataSet();
+            DataTable table = ds.Tables.Add("TBL");
+            table.Columns.Add(new DataColumn("ID", typeof(int)));
+            for (int i = 0; i < 100; i++)
+            {
+
+                var row = table.NewRow();
+                row["ID"] = i;
+                table.Rows.Add(row);
+
+            }
+            string query = "Delete from [TBL] where [ID] = 1";
+            SQLInterpreter interpreter = new SQLInterpreter(ds);
+
+            var result = interpreter.Execute(query);
+            int affected = result.RowsAffected;
+            Assert.AreEqual(1, affected, "There should be 1 row affected");
+            Assert.AreEqual(99, table.Rows.Count, "There should be 99 rows on the table");
+        }
     }
 }
