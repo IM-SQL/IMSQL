1e7b10a368586b76eade967f33fd20863b6660e1
Matias Teragni <matias.teragni@gmail.com>
2018-05-23 14:04:30 -0300

    Added tests for Update top

--- MemSQL/MemSQL.Test/ModificationTests.cs
+++ MemSQL/MemSQL.Test/ModificationTests.cs
@@ -52,10 +52,67 @@ namespace MemSQL.Test
             for (int i = 0; i < 100; i++)
             {
                 Assert.AreEqual(2, table.Rows[i]["ID"], "The updated value was not present on All the rows of the Table");
-            }
-            {
-
-            }
+            } 
         }
+
+
+        [TestMethod]
+        public void TopUpdateTest()
+        {
+            DataSet ds = new DataSet();
+            DataTable table = ds.Tables.Add("TBL");
+            table.Columns.Add(new DataColumn("ID", typeof(int)));
+            for (int i = 0; i < 200; i++)
+            {
+                var row = table.NewRow();
+                row["ID"] = i;
+                table.Rows.Add(row);
+            }
+            string query = "Update TOP(50)  [TBL] set ID=2";
+            SQLInterpreter interpreter = new SQLInterpreter(ds);
+
+            var result = interpreter.Execute(query);
+            int affected = result.RowsAffected;
+            Assert.AreEqual(50, affected, "There should be 50 row affected");
+
+            int count = 0;
+            for (int i = 0; i < 200; i++)
+            {
+                if (table.Rows[i]["ID"].Equals(2))
+                    count++;
+            }
+            Assert.AreEqual(50, affected, "There was suposed to be 50 rows with the updated ID");
+
+
+        }
+
+        [TestMethod]
+        public void TopPercentUpdateTest()
+        {
+            DataSet ds = new DataSet();
+            DataTable table = ds.Tables.Add("TBL");
+            table.Columns.Add(new DataColumn("ID", typeof(int)));
+            for (int i = 0; i < 200; i++)
+            {
+                var row = table.NewRow();
+                row["ID"] = i;
+                table.Rows.Add(row);
+            }
+            string query = "Update TOP(50) PERCENT [TBL] set ID=2";
+            SQLInterpreter interpreter = new SQLInterpreter(ds);
+
+            var result = interpreter.Execute(query);
+            int affected = result.RowsAffected;
+            Assert.AreEqual(100, affected, "There should be 100 row affected");
+            int count = 0;
+            for (int i = 0; i < 200; i++)
+            {
+                if (table.Rows[i]["ID"].Equals(2))
+                    count++;
+            }
+            Assert.AreEqual(100, affected, "There was suposed to be 100 rows with the updated ID");
+
+        }
+
     }
 }
