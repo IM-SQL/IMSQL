197ab25a0dddc4fc24b04acd9995070ec07594fa
RichoM <richi.moran@gmail.com>
2018-06-23 19:18:33 -0300

    Removed SQLFlattenVisitor and implemented Flatten() with SQLDynamicVisitor.

---
+++ IMSQL/IMSQL/Tools/Extensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.SqlServer.TransactSql.ScriptDom;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMSQL.Tools
+{
+    public static class Extensions
+    {
+        public static IEnumerable<TSqlFragment> Flatten(this TSqlFragment fragment)
+        {
+            var nodes = new List<TSqlFragment>();
+            SQLDynamicVisitor.Default(node => nodes.Add(node)).Visit(fragment);
+            return nodes;
+        }
+    }
+}
