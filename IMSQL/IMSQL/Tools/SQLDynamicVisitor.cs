c5039bb857ed182ce93390030c0139f0db4c38da
RichoM <richi.moran@gmail.com>
2018-06-23 18:44:37 -0300

    Added SQLDynamicVisitor.
    It allows to specify actions for each node type without having to build an
    entire new class. It could be useful for simple queries on the AST.

---
+++ IMSQL/IMSQL/Tools/SQLDynamicVisitor.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.SqlServer.TransactSql.ScriptDom;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMSQL.Tools
+{
+    public class SQLDynamicVisitor
+    {
+        public static SQLDynamicVisitor Default(Action<TSqlFragment> defaultAction)
+        {
+            return new SQLDynamicVisitor(defaultAction);
+        }
+        
+        private Action<TSqlFragment> defaultAction;
+        private Dictionary<Type, Action<TSqlFragment>> dispatchTable;
+
+        private SQLDynamicVisitor(Action<TSqlFragment> defaultAction)
+            : this(defaultAction, new Dictionary<Type, Action<TSqlFragment>>())
+        {}
+
+        private SQLDynamicVisitor(Action<TSqlFragment> defaultAction, Dictionary<Type, Action<TSqlFragment>> dispatchTable)
+        {
+            this.defaultAction = defaultAction;
+            this.dispatchTable = dispatchTable;
+        }
+
+        public SQLDynamicVisitor ForType<T>(Action<T> action) where T : TSqlFragment
+        {
+            var table = new Dictionary<Type, Action<TSqlFragment>>(dispatchTable);
+            table[typeof(T)] = node => action(node as T);
+            return new SQLDynamicVisitor(defaultAction, table);
+        }
+
+        public void Visit(TSqlFragment node)
+        {
+            var visitor = new SQLInternalVisitor(this);
+            node.Accept(visitor);            
+        }
+
+        class SQLInternalVisitor : TSqlFragmentVisitor
+        {
+            private SQLDynamicVisitor outer;
+
+            public SQLInternalVisitor(SQLDynamicVisitor outer)
+            {
+                this.outer = outer;
+            }
+
+            public override void Visit(TSqlFragment node)
+            {
+                Action<TSqlFragment> action;
+                if (!outer.dispatchTable.TryGetValue(node.GetType(), out action))
+                {
+                    action = outer.defaultAction;
+                }
+                action(node);
+            }
+        }
+    }
+}
